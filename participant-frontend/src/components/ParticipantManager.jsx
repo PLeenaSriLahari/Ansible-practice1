94e604833b57c6c3b0ad7dff737a463fc7562a4a
LSL <2300031012@kluniversity.in>
2025-11-06 16:19:32 +0530

    Added files

--- participant-frontend/src/components/ParticipantManager.jsx
+++ participant-frontend/src/components/ParticipantManager.jsx
@@ -20,7 +20,7 @@ const ParticipantManager = () => {
   const [message, setMessage] = useState('');
   const [editMode, setEditMode] = useState(false);
 
-  const baseUrl = `${config.url}`; 
+  const baseUrl = `${import.meta.env.VITE_API_URL}/participantapi`; 
   //   const baseUrl = `${config.url}/participantapi`;
 
   useEffect(() => {
